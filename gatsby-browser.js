62aa03d38b437f6edd8871d77fa33450aa3c783f
seven-mile <i@7li.moe>
2024-03-16 13:41:46 +0800

    init: blog ng

---
+++ gatsby-browser.js
@@ -0,0 +1,10 @@
+// custom typefaces
+import "@fontsource-variable/montserrat"
+import "@fontsource/source-serif-pro"
+// normalize CSS across browsers
+import "./src/normalize.css"
+// custom CSS styles
+import "./src/style.css"
+
+// Highlighting for code blocks
+import "prismjs/themes/prism.css"
